7fa94af63459700a5090d9b2ce4954437d5d8c17
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2023-02-07 16:56:02 -0300

    feat: resizable node

--- src/components/nodes/Square.tsx
+++ src/components/nodes/Square.tsx
@@ -1,14 +1,24 @@
 import { Handle, NodeProps, Position } from "reactflow";
+import { NodeResizer } from "@reactflow/node-resizer";
+import '@reactflow/node-resizer/dist/style.css';
 
-export function Square(props: NodeProps) {
+export function Square({ selected }: NodeProps) {
   return (
-    <div className="bg-violet-500 rounded w-[200px] h-[200px]">
+    <div className="bg-violet-500 rounded w-full h-full min-w-[200px] min-h-[200px]">
+      <NodeResizer
+        minWidth={200}
+        minHeight={200}
+        isVisible={selected}
+        lineClassName="border-blue-400"
+        handleClassName="h-3 w-3 bg-white border-2 rounded border-blue-400"
+      />
+
       <Handle
         id="right"
         type="source"
         position={Position.Right}
         className="-right-5 w-3 h-3 border-2 bg-blue-400/80"
-        />
+      />
 
       <Handle
         id="left"
